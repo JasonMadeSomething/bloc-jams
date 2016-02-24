5a01091be16e791a65869e97de7d0139073fe85c
Jason Teitelman <jason@mailmarketingfla.com>
2016-02-24 10:49:19 -0500

    Assignment 34 complete, Player bar Play-Pause

--- scripts/album.js
+++ scripts/album.js
@@ -96,6 +96,18 @@ var setVolume = function(volume){
     }
 };
 
+var togglePlayFromPlayerBar = function() {
+  if(currentSoundFile.isPaused()){
+      getSongNumberCell(currentlyPlayingSongNumber).html(pauseButtonTemplate);
+      $(this).html(playerBarPauseButton);
+      currentSoundFile.play();
+  }else{
+      getSongNumberCell(currentlyPlayingSongNumber).html(playButtonTemplate);
+      $(this).html(playerBarPlayButton);
+      currentSoundFile.pause();
+  }  
+};
+
 var setSong = function(songNumber) {
     
     if (currentSoundFile) {
@@ -167,10 +179,12 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function(){
     
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar);
 });
